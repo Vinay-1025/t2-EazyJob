37f0a910bc87f616a800126bb67e14fceda5ab25
Vinay-1025 <darkeaglerules6ios@gmail.com>
2024-12-19 22:46:54 +0530

    added the data in the Hero component and added respective styles in Hero css file

--- src/components/LandingPage/Hero/Hero.jsx
+++ src/components/LandingPage/Hero/Hero.jsx
@@ -3,7 +3,21 @@ import './Hero.css'
 const Hero = () => {
     return (
         <div className='hero-section'>
-            hero
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="hero-title">
+                            The new software era.
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="hero-desc">
+                            Non proident, sunt in culpa qui officia deserunt mollit anim id est laborum nullam labore id sed. In saepe aperiam sedid tamquam labores nec harum
+                        </div>
+                        <div className='hero-butn'>Explore !</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
